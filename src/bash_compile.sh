8d331a3a4f053ebdf7820366faef1086b2e61d78
Tris <tris.lett@gmail.com>
2016-04-13 13:43:00 +0200

    small error

--- src/bash_compile.sh
+++ src/bash_compile.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
-if [ $# -eq 0 ]; then
-	echo "Usage: `basename $0`"
-	echo "Options:"
-	echo "[-e] Set SURF_TFCE as environment variable in ~/.bashrc"
-	echo "[-m] Copy midthickness surface to fsaverage"
-	exit 1;
-fi
-
-while getopts "em" opt; do
+while getopts "emh" opt; do
 	case $opt in
 		e)
 			echo "export SURF_TFCE="`dirname $PWD` >> ~/.bashrc
 		;;
 		m)
 			cp ../adjacency_sets/?h.midthickness $SUBJECTS_DIR/fsaverage/surf/
+		;;
+		h)
+			echo "Usage: `basename $0`"
+			echo "Options:"
+			echo "[-e] Set SURF_TFCE as environment variable in ~/.bashrc"
+			echo "[-m] Copy midthickness surface to fsaverage"
+			exit 1;
 		;;
 		\?)
 		echo "Invalid option: -$OPTARG"

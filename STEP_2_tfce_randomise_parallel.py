2b124f9d70ae166af13a39bb391474e2a54fabb8
Tristram Lett <tris.lett@gmail.com>
2016-10-18 07:25:04 +0200

    common randomise parallel

--- STEP_2_tfce_randomise_parallel.py
+++ STEP_2_tfce_randomise_parallel.py
@@ -24,12 +24,9 @@ from time import time
 
 def get_script_path():
 	return os.path.dirname(os.path.realpath(sys.argv[0]))
-def round_perm_num(numperm):
-	return ((numperm + 50)/100)*100/2
 
 ap = ap.ArgumentParser(description="Permutation testing for multiple regression with TFCE on vertex data.")
 ap.add_argument("-n", "--numperm", nargs=1, type=int, help="# of permutations", metavar=('INT'), required=True)
-ap.add_argument("-s", "--surface", nargs=1, help="surface (area or thickness)", metavar=('STR'), required=True)
 ap.add_argument("-v", "--specifyvars", nargs=2, type=int, help="Optional. Specify which regressors are permuted [first] [last]. For one variable, first=last.", metavar=('INT','INT'))
 group = ap.add_mutually_exclusive_group(required=True)
 group.add_argument("-p","--gnuparallel", nargs=1, type=int, help="Use GNU parallel. Specify number of cores", metavar=('INT'))
@@ -38,13 +35,24 @@ group.add_argument("-f","--fslsub", help="Use fsl_sub script.",action="store_tru
 datatype = ap.add_mutually_exclusive_group(required=True)
 group.add_argument("--voxel", help="Voxel analysis", action="store_true")
 group.add_argument("--vertex", help="Vertex analysis [area or thickness]", nargs=1,metavar=('STR'))
-ap.add_argument("-m", "--mediation", nargs=1, help="mediation type [M or I or Y]", metavar=('STR'), required=True)
+ap.add_argument("-m", "--mediation", nargs=1, help="mediation type [M or I or Y]", metavar=('STR'))
 opts = ap.parse_args()
 
 
 SCRIPTPATH=get_script_path()
 currentTime=int(time())
 
+if opts.voxel:
+	whichScript= "%s/voxel_tfce_multiple_regression_randomise.py -s %s -r %i %i"
+		whichScript= "%s/voxel_tfce_multiple_regression_randomise.py -r %i %i -s %s -v %d %d"
+	if opts.mediation:
+		whichScript= ""
+else:
+	whichScript= "%s/vertex_tfce_multiple_regression_randomise.py -s %s -r %i %i"
+	if opts.specifyvars:
+		whichScript= "%s/vertex_tfce_multiple_regression_randomise.py -s %s -v %d %d -r %i %i"
+	if opts.mediation:
+		whichScript= ""
 roundperm=int(np.round(opts.numperm[0]/200.0)*100.0)
 forperm=(roundperm/100)-1
 print "Evaluating %d permuations" % (roundperm*2)

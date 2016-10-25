6951c74f88efe45cc7033069da55824fae826418
Tris <tris.lett@gmail.com>
2016-10-25 18:43:53 +0200

    fixed pointers

--- tfce_mediation/tools/voxel_cluster_step1.py
+++ tfce_mediation/tools/voxel_cluster_step1.py
@@ -7,9 +7,9 @@ DESCRIPTION = "Python wrapper to run FSL's cluster_results on pFWE corrected sta
 
 def getArgumentParser(ap = ap.ArgumentParser(description = DESCRIPTION)):
 	ap.add_argument("-i", "--image", 
-		help="stat_image_?h.mgh", 
+		help="stat_image.nii.gz", 
 		nargs=1, 
-		metavar=('*.mgh'), 
+		metavar=('*.nii.gz'), 
 		required=True)
 	ap.add_argument("-t", "--threshold", 
 		help="1-P(FWE) threshold (default is 0.95)", 
@@ -22,7 +22,7 @@ def run(opts):
 	statimage=str(opts.image[0])
 	thresh=opts.threshold[0]
 
-	os.system('mkdir -p cluster_results; cluster -i %s -t %1.2f --mm --scalarname="1-p" -o cluster_results/$(basename %s .nii.gz)_clusters > cluster_results/$(basename %s .nii.gz)_results' % (statimage,thresh,statimage,statimage))
+	os.system('mkdir -p cluster_results; $FSLDIR/bin/cluster -i %s -t %1.2f --mm --scalarname="1-p" -o cluster_results/$(basename %s .nii.gz)_clusters > cluster_results/$(basename %s .nii.gz)_results' % (statimage,thresh,statimage,statimage))
 
 if __name__ == "__main__":
 	parser = getArgumentParser()
